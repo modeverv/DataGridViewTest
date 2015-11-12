b040eaa57d69d4624b3ea366b23a1f26947deea1
norainu <test@example.com>
2015-11-12 16:39:00 +0900

    画面表示まで。

---
+++ DataGridViewTest/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewTest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.DataSource = getUserList();
+        }
+
+        private List<User> getUserList()
+        {
+            List<User> list = new List<User>();
+            for(int i = 0; i < 100; i++)
+            {
+                list.Add(new User(i, "name" + i.ToString()));
+            }
+            return list;
+        }
+    }
+
+    public class User
+    {
+        public int id
+        {
+            get; set;
+        }
+        public string name
+        {
+            get; set;
+        }
+
+        public User(int id,string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
